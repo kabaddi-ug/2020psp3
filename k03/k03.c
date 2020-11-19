7e3e1d1ce4dd0ceb00903d9462ac64f68e49aa5e
瀧井祐自 <bb35319026@ms.nagasaki-u.ac.jp>
2020-11-19 11:48:58 +0900

    提出１回目

--- k03/k03.c
+++ k03/k03.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 #define DEBUG
 
@@ -9,14 +10,54 @@ char StrKey[] = "wind";
 
 char* ForceSearch(char text[], char key[])
 {
-    //  ここを実装する
+    int start, pos, text_len=strlen(text), key_len=strlen(key); 
+    for(start=0; start<(text_len-key_len); start++){
+        for(pos=0; pos<key_len; pos++){
+            if(text[start+pos]==key[pos]){
+                if(pos==key_len-1){
+                   return  text+start;
+                }else{
 
+                }
+            }else{
+                break;
+            }
+        }
+    }
+return NULL;
 }
 
 char* BMSearch(char text[], char key[])
 {
-    //  ここを実装する
+    int index, key_index, text_len=strlen(text), key_len=strlen(key),table[256];
+    for(index=0; index<256; index++){
+        table[index]=key_len;
+    }
+    for(index=0; index<key_len; index++){
+        int pos = key_len-index-1;
+        char c = key[index];
+        table[(int)c]=pos;
+    }
+    index = key_len - 1;
+    while(index<text_len){
+        for(key_index=key_len-1; key_index>=0; key_index--, index--){
+            if(text[index]==key[key_index]){
+                if(key_index==0){
+                    return text+index;
+                }
+            }else{
+                break;
+            }
+        }
 
+        int new_index=index+table[(int)text[index]];
+        if(new_index<=index){
+            index++;
+        } else{
+            index=new_index;
+        }
+    }
+    return NULL;
 }
 
 int main(void)
